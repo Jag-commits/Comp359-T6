5fdd74dec2742d4c8e258490df0ecfc6586e91bb
Jag-commits <jagpreetdhaliwal06@gmail.com>
2026-02-07 18:14:17 -0800

    Small fixes + more efficient loops -Jagpreet

--- TopSort.py
+++ TopSort.py
@@ -4,22 +4,30 @@ class TopSort:
     #Referencing Kahn's BFS Topological Sorting Algorithm, Just the concept, not the code
     #We decided on using adjacency lists from researching Kahn's algo -> dictionaries are easier to iterate through compared to 2d lists
 
-    #I'm writing this code before my partners and I want to ensure only the adjacency list gets passed through
+    #I'm writing this code before my partners and I just want to ensure only the adjacency list gets passed through
     def topologicalSort(inputList: dict) -> list:
-        numOfNodes = len(inputList)
+        sortedList = []
         #How many nodes are pointing at a specific node is called in-degree
         inDegree = {}
         #Kahn's algo uses BFS, which I learned by using queues
-        queue = deque()
-        
-        #Set all the indegrees up -> I need a more efficient way
-        for Node in inputList:
-            inDegree[Node]=0
+        bfsQueue = deque()
 
+        #Reminder to self -> Don't forget to explain why top sort needs in-degrees first
+        #Iterate through Nodes and add their associated in-degrees.
         for Node in inputList:
+            inDegree.setdefault(Node,0)
             for connectedNodes in inputList[Node]:
-                inDegree[Node] +=1
+                inDegree.setdefault(connectedNodes,0)
+                inDegree[connectedNodes] +=1
         
-        for Node in inputList:
+        for Node in inDegree:
+            #If the in-degree is 0, this can be read first as nothing points to it
             if (inDegree[Node]==0):
-                queue.append
+                bfsQueue.append(Node)
+        
+
+        
+
+        
+
+        
