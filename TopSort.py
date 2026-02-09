88acb64f40d1c81a4da1f9be1c2013fe5509fd0a
Jag-commits <jagpreetdhaliwal06@gmail.com>
2026-02-09 08:20:59 -0800

    Made sorting algorithm for reading order -Jagpreet

--- TopSort.py
+++ TopSort.py
@@ -21,10 +21,35 @@ class TopSort:
                 inDegree[connectedNodes] +=1
         
         for Node in inDegree:
-            #If the in-degree is 0, this can be read first as nothing points to it
+            #If the in-degree is 0, this can be added first as nothing points to it
+            #BFS needs parent nodes to start with
             if (inDegree[Node]==0):
                 bfsQueue.append(Node)
         
+
+        while (bfsQueue):
+            #So it'll pop the first node, add it's children to the queue.
+            firstNode = bfsQueue.popleft()
+            #Edge case where the list somehow has the node as a dependency, but not as a key
+            for connectedNodes in inputList.get(firstNode,[]):
+                #We just popped the connected node, so it has 1 less incoming edge ie reduce indegrees by 1.
+                inDegree[connectedNodes] -=1
+                #This paper no longer has any incoming edges left->The paper that was citing this has already been added
+                if inDegree[connectedNodes]==0:
+                    bfsQueue.append(connectedNodes)
+                else:
+                    continue
+            sortedList.append(firstNode)
+
+        #My logic was that the papers that were cited the most should be foundational knowledge for papers that have no or few citations
+        #From Kahn's algo, the papers that are not cited by anything ie indegree =0 come first.
+        return sortedList[::-1]
+
+    inputList = {"D": ["B", "C"],"B": ["A"],"C": ["A"],"A": []}
+    #The list doesn't have A as key test case
+    inputList2 = {"D": ["B", "C"],"B": ["A"],"C": ["A"]}
+    print(topologicalSort(inputList))
+    print(topologicalSort(inputList2))
 
         
 
